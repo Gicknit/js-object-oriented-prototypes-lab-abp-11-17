a70f194c652cc119694ebdf089b030181ec4b452
Nick Garcia <89nickgarcia@gmail.com>
2017-11-12 22:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function boardMember(name, homeState, training){
+function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
 
   //Change this to user.prototype//
-  boardMember.prototype.veto = function(){
+  BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
   }
-  boardMember.prototype.approve = function(){
+  BoardMember.prototype.approve = function(){
     return 'You can do that!'
   }
-  boardMember.prototype.doCharity = function(){
+  BoardMember.prototype.doCharity = function(){
     return "I like to help people."
   }
 
-  boardMember.prototype.releasePressStatement = function(){
+  BoardMember.prototype.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
 
-  boardMember.prototype.sayHi = function(){
+  BoardMember.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
